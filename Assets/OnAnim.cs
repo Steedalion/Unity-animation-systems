fdbcdf13b7fc5b5fc162659dd323f505465d4a13
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-02-08 16:21:49 +0900

    Used OnAnimatorMove() method.

---
+++ Assets/OnAnim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnAnim : MonoBehaviour
+{
+    public Animator animator;
+    private void OnAnimatorMove()
+    {
+        transform.position += animator.deltaPosition;
+        transform.rotation *= animator.deltaRotation;
+    }
+}
