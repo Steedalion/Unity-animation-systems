6484944f826b9915cbb23f31faed88599b36242b
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-02-08 17:59:07 +0900

    Crouching controller implemented.

---
+++ Assets/PlayerController3D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour
+{
+    public float speed, turnSpeed;
+    static readonly int speedHash = Animator.StringToHash("Speed");
+    static readonly int turnSpeedHash = Animator.StringToHash("TurningRate");
+
+    public Animator animator;
+
+    private void Start()
+    {
+        StartCoroutine(SetAnimatorSpeeds());
+    }
+
+
+    void Update()
+    {
+        turnSpeed = Input.GetAxis("Horizontal");
+        speed = Input.GetAxis("Vertical");
+    }
+
+    IEnumerator SetAnimatorSpeeds()
+    {
+        while (true)
+        {
+            animator.SetFloat(speedHash, speed);
+            animator.SetFloat(turnSpeedHash, turnSpeed);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
